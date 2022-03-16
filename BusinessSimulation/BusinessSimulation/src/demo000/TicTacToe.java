ec68d801db9600a8b1aede8eb55ac15868f064eb
MarioNeugebauer <48792182+MarioNeugebauer@users.noreply.github.com>
2022-03-16 18:13:28 +0100

    added tictactoe example code

---
+++ BusinessSimulation/BusinessSimulation/src/demo000/TicTacToe.java
@@ -0,0 +1,41 @@
+package demo000;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		
+	    Scanner myScanner = new Scanner(System.in);
+
+		// Variable, um die mit scanf eingebene Feldnummer zu speichern
+		int feldEingegeben=0;
+
+		// true setzen, falls Integer eingegeben wurde, ansonsten false
+		boolean scannerErfolg=false;
+		
+
+		// ...
+		
+		while(!scannerErfolg) {
+			try {
+			    System.out.println("Feldnummer als Integer eingeben:");
+			    feldEingegeben = myScanner.nextInt();
+				scannerErfolg=true;
+			} catch (Exception e) {
+				scannerErfolg=false;
+				myScanner.nextLine();
+			}
+		}
+
+		// nur zum Test
+		System.out.println(feldEingegeben);
+		
+		// ab hier steht in feldEingegeben die Feldnummer, welche der Spieler eingegeben hat.
+
+		// ...
+
+
+	}
+
+}
