584150cbefeae6b40ee5472327ab5989f1d3a140
MarioNeugebauer <48792182+MarioNeugebauer@users.noreply.github.com>
2022-03-16 17:38:46 +0100

    update ScannerInput, Zahlenratespiel

--- BusinessSimulation/BusinessSimulation/src/demo000/ScannerInput.java
+++ BusinessSimulation/BusinessSimulation/src/demo000/ScannerInput.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 
 class ScannerInput {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
+    Scanner myScanner = new Scanner(System.in);
 
     System.out.println("Bitte eine Zeichenkette eingeben:");
-    String zeichenkette = myObj.nextLine();
+    String zeichenkette = myScanner.nextLine();
     
     System.out.println("Bitte einen Integer eingeben:");
-    int einInteger = myObj.nextInt();
+    int einInteger = myScanner.nextInt();
 
     // Output input by user
     System.out.println("Zeichenkette: " + zeichenkette);
     System.out.println("Ein Integer: " + einInteger);
+    
 
-    myObj.close();
+    myScanner.close();
   }
 }
---
+++ BusinessSimulation/BusinessSimulation/src/demo000/Zahlenratespiel.java
@@ -0,0 +1,24 @@
+package demo000;
+
+import java.util.Scanner;
+
+public class Zahlenratespiel {
+
+	public static void main(String[] args) {
+		
+	    Scanner myScanner = new Scanner(System.in);
+	    
+		int randomInt = (int)(Math.random()*100);
+		int zahlGeraten;
+
+		// ...
+		
+	    System.out.println("Bitte raten!");
+	    int einInteger = myScanner.nextInt();
+	    
+	    // ...
+
+
+	}
+
+}
