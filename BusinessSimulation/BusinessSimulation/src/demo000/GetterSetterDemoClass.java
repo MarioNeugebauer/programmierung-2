013a9fc42ead5148db20a0e45ae47d16ffb4ed8b
MarioNeugebauer <48792182+MarioNeugebauer@users.noreply.github.com>
2022-04-19 05:44:42 +0200

    Create GetterSetterDemoClass.java

---
+++ BusinessSimulation/BusinessSimulation/src/demo000/GetterSetterDemoClass.java
@@ -0,0 +1,17 @@
+package demo000;
+
+public class GetterSetterDemoClass {
+	private int einIntAttribut;
+	
+	public GetterSetterDemoClass(int value) {
+		einIntAttribut = value;
+	}
+
+	public int getEinIntAttribut() {
+		return einIntAttribut;
+	}
+
+	public void setEinIntAttribut(int einIntAttribut) {
+		this.einIntAttribut = einIntAttribut;
+	}
+}
